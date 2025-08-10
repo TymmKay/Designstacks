1224fb2a8e817fa41f47ca67d8e443f3ac57fc40
Timothy <126267251+TymmKay@users.noreply.github.com>
2025-08-10 16:39:31 +0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,14 +9,12 @@ function withOpacity(variableName) {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class", // ✅ enable class-based dark mode
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    // Remove the following screen breakpoint or add other breakpoints
-    // if one breakpoint is not enough for you
     screens: {
       sm: "640px",
     },
-
     extend: {
       textColor: {
         skin: {
@@ -57,7 +55,6 @@ module.exports = {
         mono: ["IBM Plex Mono", "monospace"],
         poppins: ["Poppins", "sans-serif"],
       },
-
       typography: {
         DEFAULT: {
           css: {
